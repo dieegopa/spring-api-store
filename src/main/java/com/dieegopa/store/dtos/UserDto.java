ff9e8fa880e9006c90fe3a2d129464e0340366af
Diego Padilla <diegopadilla6@gmail.com>
2025-07-13 13:26:03 +0200

    Add json examples

--- src/main/java/com/dieegopa/store/dtos/UserDto.java
+++ src/main/java/com/dieegopa/store/dtos/UserDto.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    // @JsonProperty("id")
+    // @JsonInclude(JsonInclude.Include.NON_NULL)
+    // @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private Long id;
     private String name;
     private String email;
