b4f93b0ef1a2f231f66b89873554dfd9f612d037
Diego Padilla <diegopadilla6@gmail.com>
2025-07-15 23:19:04 +0200

    Add crud operations for product

--- src/main/java/com/dieegopa/store/dtos/ProductDto.java
+++ src/main/java/com/dieegopa/store/dtos/ProductDto.java
@@ -2,10 +2,12 @@ package com.dieegopa.store.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class ProductDto {
     private Long id;
