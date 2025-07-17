4c6fdb72b61e941f7f77d4e88fdfe9e0a4b07d64
Diego Padilla <diegopadilla6@gmail.com>
2025-07-17 14:18:10 +0200

    Add login endpoint

---
+++ src/main/java/com/dieegopa/store/dtos/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.dieegopa.store.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotNull
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotNull
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
--- src/main/java/com/dieegopa/store/repositories/UserRepository.java
+++ src/main/java/com/dieegopa/store/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.dieegopa.store.repositories;
 
 import com.dieegopa.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
