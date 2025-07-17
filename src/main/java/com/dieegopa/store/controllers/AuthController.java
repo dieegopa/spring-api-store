93195b77ee00bcca0d3787dca51248fffebdd40b
Diego Padilla <diegopadilla6@gmail.com>
2025-07-17 21:51:00 +0200

    Add me endpoint to get user info

--- src/main/java/com/dieegopa/store/controllers/AuthController.java
+++ src/main/java/com/dieegopa/store/controllers/AuthController.java
@@ -2,12 +2,16 @@ package com.dieegopa.store.controllers;
 
 import com.dieegopa.store.dtos.JwtResponse;
 import com.dieegopa.store.dtos.LoginRequest;
+import com.dieegopa.store.dtos.UserDto;
+import com.dieegopa.store.mappers.UserMapper;
+import com.dieegopa.store.repositories.UserRepository;
 import com.dieegopa.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -17,6 +21,8 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -41,5 +47,19 @@ public class AuthController {
     ) {
         var token = authHeader.replace("Bearer ", "");
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+        var user = userRepository.findByEmail(email).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 }
