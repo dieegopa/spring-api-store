de5f72405d2e0da1163f921062f0bfd12e73dce6
Diego Padilla <diegopadilla6@gmail.com>
2025-07-23 00:08:24 +0200

    Add stripe session creation

--- src/main/java/com/dieegopa/store/controllers/CheckoutController.java
+++ src/main/java/com/dieegopa/store/controllers/CheckoutController.java
@@ -1,12 +1,15 @@
 package com.dieegopa.store.controllers;
 
 import com.dieegopa.store.dtos.CheckoutRequest;
-import com.dieegopa.store.dtos.CheckoutResponse;
+import com.dieegopa.store.dtos.ErrorDto;
 import com.dieegopa.store.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +28,15 @@ public class CheckoutController {
             summary = "Checkout",
             description = "Processes the checkout for a given order. Requires a valid CheckoutRequest."
     )
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
-        return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request) {
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+        } catch (StripeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(
+                            new ErrorDto("Error creating checkout session")
+                    );
+        }
     }
 
 }
--- src/main/java/com/dieegopa/store/services/CheckoutService.java
+++ src/main/java/com/dieegopa/store/services/CheckoutService.java
@@ -7,10 +7,17 @@ import com.dieegopa.store.exceptions.CartEmptyException;
 import com.dieegopa.store.exceptions.CartNotFoundException;
 import com.dieegopa.store.repositories.CartRepository;
 import com.dieegopa.store.repositories.OrderRepository;
-import lombok.AllArgsConstructor;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
 @Service
 public class CheckoutService {
 
@@ -19,7 +26,11 @@ public class CheckoutService {
     private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
 
-    public CheckoutResponse checkout(CheckoutRequest request) {
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    @Transactional
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) {
             throw new CartNotFoundException();
@@ -31,9 +42,45 @@ public class CheckoutService {
 
         var order = Order.fromCart(cart, authService.getCurrentUser());
         orderRepository.save(order);
-        cartService.clearCart(cart.getId());
 
-        return new CheckoutResponse(order.getId());
+        // Create a checkout session
+        try {
+            var builder = SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                    .setCancelUrl(websiteUrl + "/checkout-cancelled");
+
+            order.getItems().forEach(item -> {
+                var lineItem = SessionCreateParams.LineItem.builder()
+                        .setQuantity(Long.valueOf(item.getQuantity()))
+                        .setPriceData(
+                                SessionCreateParams.LineItem.PriceData.builder()
+                                        .setCurrency("eur")
+                                        .setUnitAmountDecimal(
+                                                item.getUnitPrice().multiply(BigDecimal.valueOf(100))
+                                        )
+                                        .setProductData(
+                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                        .setName(item.getProduct().getName())
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+
+                builder.addLineItem(lineItem);
+            });
+
+            var session = Session.create(builder.build());
+
+            cartService.clearCart(cart.getId());
+
+            return new CheckoutResponse(order.getId(), session.getUrl());
+        } catch (StripeException e) {
+            System.out.println(e.getMessage());
+            orderRepository.delete(order);
+            throw e;
+        }
     }
 
 }
