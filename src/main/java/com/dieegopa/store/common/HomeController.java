33b172757ac3873491dac4b5eed0e877bff99073
Diego Padilla <diegopadilla6@gmail.com>
2025-07-24 15:11:03 +0200

    Redirect to swagger

---
+++ src/main/java/com/dieegopa/store/common/HomeController.java
@@ -0,0 +1,12 @@
+package com.dieegopa.store.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
