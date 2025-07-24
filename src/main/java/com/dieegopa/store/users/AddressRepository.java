917e596228acabfcfe177efb354468a392c6e80c
Diego Padilla <diegopadilla6@gmail.com>
2025-07-24 11:55:18 +0200

    Move address repository

--- src/main/java/com/dieegopa/store/users/AddressRepository.java
+++ src/main/java/com/dieegopa/store/users/AddressRepository.java
@@ -1,6 +1,5 @@
-package com.dieegopa.store.repositories;
+package com.dieegopa.store.users;
 
-import com.dieegopa.store.users.Address;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AddressRepository extends CrudRepository<Address, Long> {
