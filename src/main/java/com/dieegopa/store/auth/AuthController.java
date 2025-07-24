fbec5ae84de6edfffe975fbd566cc622b0ecc5f3
Diego Padilla <diegopadilla6@gmail.com>
2025-07-24 12:02:25 +0200

    Refactor auth controller

--- src/main/java/com/dieegopa/store/auth/AuthController.java
+++ src/main/java/com/dieegopa/store/auth/AuthController.java
@@ -2,18 +2,13 @@ package com.dieegopa.store.auth;
 
 import com.dieegopa.store.users.UserDto;
 import com.dieegopa.store.users.UserMapper;
-import com.dieegopa.store.users.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -22,41 +17,31 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Authentication", description = "Operations related to user authentication")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtService jwtService;
-    private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final JwtConfig jwtConfig;
+    private final AuthService authService;
 
     @PostMapping("/login")
     @Operation(
             summary = "User login",
             description = "Authenticates a user and returns a JWT access token and a refresh token."
     )
-    public ResponseEntity<JwtResponse> login(
+    public JwtResponse login(
             @Valid @RequestBody LoginRequest request,
             HttpServletResponse response
     ) {
 
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
+        var loginResult = authService.login(request);
 
-        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
-        var accessToken = jwtService.generateAccessToken(user);
-        var refreshToken = jwtService.generateRefreshToken(user);
-
-        var cookie = new Cookie("refreshToken", refreshToken.toString());
+        var refreshToken = loginResult.getRefreshToken().toString();
+        var cookie = new Cookie("refreshToken", refreshToken);
         cookie.setHttpOnly(true);
         cookie.setPath("/auth/refresh");
         cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
         cookie.setSecure(true);
         response.addCookie(cookie);
 
-        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
+        return new JwtResponse(loginResult.getAccessToken().toString());
     }
 
     @PostMapping("/refresh")
@@ -64,19 +49,11 @@ public class AuthController {
             summary = "Refresh access token",
             description = "Generates a new access token using the provided refresh token."
     )
-    public ResponseEntity<JwtResponse> refresh(
+    public JwtResponse refresh(
             @CookieValue(value = "refreshToken") String refreshToken
     ) {
-        var jwt = jwtService.parseToken(refreshToken);
-        if (jwt == null || jwt.isExpired()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        var user = userRepository.findById(jwt.getUserId()).orElseThrow();
-        var accessToken = jwtService.generateAccessToken(user);
-
-        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
-
+        var accessToken = authService.refreshAccessToken(refreshToken);
+        return new JwtResponse(accessToken.toString());
     }
 
     @GetMapping("/me")
@@ -85,10 +62,7 @@ public class AuthController {
             description = "Retrieves the details of the currently authenticated user."
     )
     public ResponseEntity<UserDto> me() {
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var userId = (Long) authentication.getPrincipal();
-        var user = userRepository.findById(userId).orElse(null);
-
+        var user = authService.getCurrentUser();
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
--- src/main/java/com/dieegopa/store/auth/AuthService.java
+++ src/main/java/com/dieegopa/store/auth/AuthService.java
@@ -3,6 +3,9 @@ package com.dieegopa.store.auth;
 import com.dieegopa.store.users.User;
 import com.dieegopa.store.users.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +14,37 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         var userId = (Long) authentication.getPrincipal();
         return userRepository.findById(userId).orElse(null);
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var accessToken = jwtService.generateAccessToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+
+        return new LoginResponse(accessToken, refreshToken);
+    }
+
+    public Jwt refreshAccessToken(String refreshToken) {
+        var jwt = jwtService.parseToken(refreshToken);
+        if (jwt == null || jwt.isExpired()) {
+            throw new BadCredentialsException("Invalid refresh token");
+        }
+
+        var user = userRepository.findById(jwt.getUserId()).orElseThrow();
+        return jwtService.generateAccessToken(user);
     }
 }
